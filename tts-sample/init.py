5a4ba623cf963fb75b3abbd6acb0a085a46e06e6
unknown <aslanchtan@gmail.com>
2025-07-15 13:41:43 +0800

    first commit

---
+++ tts-sample/init.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+webUrl = "https://hktts.bronci.com.tw/"
+ttsUrl = "https://hktts.bronci.com.tw/"
+username = "hackathon2025_039"
+password = "!EVuy&4Oq516"
+transFile = "test.txt"
+# transFile = "tts-sample/1s.txt"
+threadCount = 6
